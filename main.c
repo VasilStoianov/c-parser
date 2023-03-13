c498285095038045c048a96baf822fb9c5d17d5d
lays <vaseto.stoianov.94@gmail.com>
2023-03-14 00:07:21 +0200

    Starting of impl of stack for search algorithm(DFS)

--- main.c
+++ main.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
 #define HELP_COMMAND "-help"
+#define SEARCH_COMMAND "-search"
 
 void parse_args(char *arguments[]);
 int string_compare(char *str1, char *str2);
+int search_file(char *file);
 
 int main(int argc, char *argv[])
 {
-    parse_args(argv);
+ //   parse_args(argv);
+    search_file("something");
     return 1;
 }
 
@@ -18,8 +23,12 @@ void parse_args(char *arguments[]){
     while(arguments[count] != NULL){
         if(string_compare(arguments[count],HELP_COMMAND) == 1){
             printf("-v helpping \n-o something else\n ");//TODO fix the message
-                    break;
         };
+        if(string_compare(arguments[count], SEARCH_COMMAND) == 1){
+           printf("SEARCHING FOR %s",arguments[count+1]);//TODO fix the search  and add search types etc dir,file
+
+        }
+        
         count++;
     }
 
@@ -38,4 +47,30 @@ int string_compare(char *str1, char *str2){
      };
      return 1;
 
+}
+
+int search_file(char *file){
+    struct dirent *de;
+    DIR *dir = opendir(".");
+     if (dir == NULL)  // opendir returns NULL if couldn't open directory
+    {
+        printf("Could not open current directory" );
+        return 0;
+    }
+
+    
+    return 1;
+}
+
+
+int iterate_and_compare(struct DIR *dir,char *file){
+    struct dirent *de;
+    while((de = readdir(dir)) != NULL){
+        printf("COMPARING\n");
+       if(string_compare(de->d_name, file) == 1) {
+        printf("File found");
+        break;
+       }
+
+    }
 }
---
+++ stack.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+typedef struct stack
+{
+    int values[15];
+    int length;
+} stack;
+
+
+
+
+
+
+
